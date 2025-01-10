665e77f19c1f6604f8add324c7309c4bb480b283
FrancisJ04 <frajvelo@espol.edu.ec>
2025-01-09 23:23:09 -0500

    Procedures y triggers tablas(Conductores, Vehículos, Auxiliares, unidades_trabajo)

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -13,3 +13,176 @@ begin
     insert into movimientos (fecha, cantidad, salida, id_almacen, id_producto) values (now(), in_cantidad, 0, in_id_almacen, id_producto);
 end ;
 // delimiter ;
+
+
+DELIMITER //
+CREATE PROCEDURE AddConductor (IN p_cedula INT, IN p_primer_nombre VARCHAR(50), IN p_segundo_nombre VARCHAR(50), IN p_primer_apellido VARCHAR(50),
+    IN p_segundo_apellido VARCHAR(50), IN p_telefono VARCHAR(15), IN p_correo VARCHAR(100), IN p_licencia VARCHAR(50), IN p_experiencia VARCHAR(50)
+)
+BEGIN
+    INSERT INTO Conductores 
+    VALUES (p_cedula, p_primer_nombre, p_segundo_nombre, p_primer_apellido, p_segundo_apellido, p_telefono, p_correo, p_licencia, p_experiencia);
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetConductor (IN p_cedula INT)
+BEGIN
+    SELECT * FROM Conductores WHERE cedula = p_cedula;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateConductor (IN p_cedula INT, IN p_primer_nombre VARCHAR(50), IN p_segundo_nombre VARCHAR(50), IN p_primer_apellido VARCHAR(50),
+    IN p_segundo_apellido VARCHAR(50), IN p_telefono VARCHAR(15), IN p_correo VARCHAR(100), IN p_licencia VARCHAR(50), IN p_experiencia VARCHAR(50)
+)
+BEGIN
+    UPDATE Conductores
+    SET primer_nombre = p_primer_nombre,
+        segundo_nombre = p_segundo_nombre,
+        primer_apellido = p_primer_apellido,
+        segundo_apellido = p_segundo_apellido,
+        telefono = p_telefono,
+        correo = p_correo,
+        licencia = p_licencia,
+        experiencia = p_experiencia
+    WHERE cedula = p_cedula;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteConductor (IN p_cedula INT)
+BEGIN
+    DELETE FROM Conductores WHERE cedula = p_cedula;
+END;
+// DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE AddAuxiliar (IN p_cedula INT, IN p_primer_nombre VARCHAR(50), IN p_segundo_nombre VARCHAR(50), IN p_primer_apellido VARCHAR(50),
+    IN p_segundo_apellido VARCHAR(50), IN p_telefono VARCHAR(15), IN p_correo VARCHAR(100)
+)
+BEGIN
+    INSERT INTO Auxiliares 
+    VALUES (p_cedula, p_primer_nombre, p_segundo_nombre, p_primer_apellido, p_segundo_apellido, p_telefono, p_correo);
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetAuxiliar (IN p_cedula INT)
+BEGIN
+    SELECT * FROM Auxiliares WHERE cedula = p_cedula;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateAuxiliar (IN p_cedula INT, IN p_primer_nombre VARCHAR(50), IN p_segundo_nombre VARCHAR(50), IN p_primer_apellido VARCHAR(50),
+    IN p_segundo_apellido VARCHAR(50), IN p_telefono VARCHAR(15), IN p_correo VARCHAR(100)
+)
+BEGIN
+    UPDATE Auxiliares
+    SET primer_nombre = p_primer_nombre,
+        segundo_nombre = p_segundo_nombre,
+        primer_apellido = p_primer_apellido,
+        segundo_apellido = p_segundo_apellido,
+        telefono = p_telefono,
+        correo = p_correo
+    WHERE cedula = p_cedula;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteAuxiliar (IN p_cedula INT)
+BEGIN
+    DELETE FROM Auxiliares WHERE cedula = p_cedula;
+END;
+// DELIMITER ;
+
+
+DELIMITER //
+CREATE PROCEDURE AddVehiculo (IN p_placa VARCHAR(10), IN p_capacidad DOUBLE, IN p_datalogger VARCHAR(50),
+    IN p_F_U_Mantenimiento DATETIME, IN p_ID_seguro INT
+)
+BEGIN
+    INSERT INTO Vehículos 
+    VALUES (p_placa, p_capacidad, p_datalogger, p_F_U_Mantenimiento, p_ID_seguro);
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetVehiculo (IN p_placa VARCHAR(10))
+BEGIN
+    SELECT * FROM Vehículos WHERE placa = p_placa;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateVehiculo (IN p_placa VARCHAR(10), IN p_capacidad DOUBLE, IN p_datalogger VARCHAR(50),
+    IN p_F_U_Mantenimiento DATETIME, IN p_ID_seguro INT
+)
+BEGIN
+    UPDATE Vehículos
+    SET capacidad = p_capacidad,
+        datalogger = p_datalogger,
+        F_U_Mantenimiento = p_F_U_Mantenimiento,
+        ID_seguro = p_ID_seguro
+    WHERE placa = p_placa;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteVehiculo (IN p_placa VARCHAR(10))
+BEGIN
+    DELETE FROM Vehículos WHERE placa = p_placa;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE AddUnidadTrabajo (
+    IN p_cedula_c INT,
+    IN p_cedula_a INT,
+    IN p_placa VARCHAR(10),
+    IN p_placa_r VARCHAR(10)
+)
+BEGIN
+    INSERT INTO unidades_trabajo 
+    VALUES (p_cedula_c, p_cedula_a, p_placa, p_placa_r);
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE GetUnidadTrabajo (
+    IN p_cedula_c INT,
+    IN p_cedula_a INT
+)
+BEGIN
+    SELECT * FROM unidades_trabajo 
+    WHERE cedula_c = p_cedula_c AND cedula_a = p_cedula_a;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE UpdateUnidadTrabajo (
+    IN p_cedula_c INT,
+    IN p_cedula_a INT,
+    IN p_placa VARCHAR(10),
+    IN p_placa_r VARCHAR(10)
+)
+BEGIN
+    UPDATE unidades_trabajo
+    SET placa = p_placa,
+        placa_r = p_placa_r
+    WHERE cedula_c = p_cedula_c AND cedula_a = p_cedula_a;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE PROCEDURE DeleteUnidadTrabajo (
+    IN p_cedula_c INT,
+    IN p_cedula_a INT
+)
+BEGIN
+    DELETE FROM unidades_trabajo 
+    WHERE cedula_c = p_cedula_c AND cedula_a = p_cedula_a;
+END;
+// DELIMITER ;
--- sql/triggers.sql
+++ sql/triggers.sql
@@ -15,3 +15,56 @@ begin
     where id_almacen = new.id_almacen and id_producto = new.id_producto;
 end; //
 delimiter ;
+
+DELIMITER //
+CREATE TRIGGER BeforeInsertConductor
+BEFORE INSERT ON Conductores
+FOR EACH ROW
+BEGIN
+    IF EXISTS (SELECT 1 FROM Conductores WHERE cedula = NEW.cedula) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cédula duplicada en tabla Conductores';
+    END IF;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER BeforeInsertAuxiliar
+BEFORE INSERT ON Auxiliares
+FOR EACH ROW
+BEGIN
+    IF EXISTS (SELECT 1 FROM Auxiliares WHERE cedula = NEW.cedula) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Cédula duplicada en tabla Auxiliares';
+    END IF;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER BeforeInsertVehiculo
+BEFORE INSERT ON Vehículos
+FOR EACH ROW
+BEGIN
+    IF EXISTS (SELECT 1 FROM Vehículos WHERE placa = NEW.placa) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Placa duplicada en tabla Vehículos';
+    END IF;
+END;
+// DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER BeforeInsertUnidadTrabajo
+BEFORE INSERT ON unidades_trabajo
+FOR EACH ROW
+BEGIN
+    IF EXISTS (
+        SELECT 1 FROM unidades_trabajo 
+        WHERE cedula_c = NEW.cedula_c 
+          AND cedula_a = NEW.cedula_a 
+          AND (placa = NEW.placa OR placa_r = NEW.placa_r)
+    ) THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Registro duplicado en tabla unidades_trabajo';
+    END IF;
+END;
+// DELIMITER ;
